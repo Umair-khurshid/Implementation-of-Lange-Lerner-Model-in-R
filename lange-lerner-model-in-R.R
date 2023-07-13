cf33cbe5427935fc46ac0cf0db8dbb078d6dbdbc
Umair-khurshid <87898890+Umair-khurshid@users.noreply.github.com>
2023-07-12 19:36:27 -0700

    Create lange-lerner-model-in-R.R
    A simple Lange-Lerner model in R

---
+++ lange-lerner-model-in-R.R
@@ -0,0 +1,46 @@
+# Step 1: Define the parameters
+num_goods <- 2
+num_firms <- 2
+num_households <- 2
+
+# Step 2: Define the initial allocation
+initial_resources <- c(100, 200)
+initial_goods <- matrix(c(50, 30, 40, 60), ncol = num_goods)
+
+# Step 3: Set up the optimization problem
+objective_function <- function(prices) {
+  # Calculate social welfare using prices and allocations
+  welfare <- sum(prices * colSums(initial_goods))
+  return(-welfare)  # Maximizing welfare, so negative sign
+}
+
+# Constraints: Production and resource constraints
+constraint_function <- function(x) {
+  # Extract production levels and resources from x
+  production <- matrix(x[1:(num_firms * num_goods)], ncol = num_goods)
+  resources <- x[(num_firms * num_goods + 1):(num_firms * num_goods + num_goods)]
+  
+  # Calculate resource constraints
+  resource_constraints <- resources - colSums(production)
+  
+  # Return a vector of constraint violations
+  return(c(resource_constraints))
+}
+
+# Step 4: Solve the optimization problem
+initial_guess <- c(initial_goods, initial_resources)
+result <- optim(initial_guess, objective_function, 
+                NULL, method = "Nelder-Mead", 
+                constraints = list(ineqfun = constraint_function),
+                control = list(fnscale = -1))  # Maximize objective function
+
+# Step 5: Update prices and allocations
+final_goods <- matrix(result$par[1:(num_firms * num_goods)], ncol = num_goods)
+final_resources <- result$par[(num_firms * num_goods + 1):(num_firms * num_goods + num_goods)]
+prices <- result$value / colSums(final_goods)
+
+# Step 6: Repeat steps 4 and 5 (optional)
+
+# Step 7: Analyze results
+cat("Final prices:", prices, "\n")
+cat("Final goods allocation:\n", final_goods)
